a930a07364a1423323bc1d8f2e5f1aab6b253714
zzm2224247288 <42564027+zzm2224247288@users.noreply.github.com>
2018-10-04 15:19:56 +0800

    Update Demo.java

--- src/main/java/com/test/test/Demo.java
+++ src/main/java/com/test/test/Demo.java
@@ -2,11 +2,16 @@ package com.test.test;
 
 import org.springframework.stereotype.Controller;
 
-@Controller
+@RestController
 public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
+	}
+	
+	@GetMapping("/")
+	public String test(){
+		return "test";
 	}
 	
 }
